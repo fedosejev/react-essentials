a0afbb3232b0507abcd854ea4cc26649995598ab
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-08-27 23:29:57 +0100

    Add Chapter 2 code

---
+++ code/chapter02/snapterest/source/app.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var listOfItems = <ul className="list-of-items">
+                   <li className="item-1">Item 1</li>
+                   <li className="item-2">Item 2</li>
+                   <li className="item-3">Item 3</li>
+                  </ul>;
+
+ReactDOM.render(listOfItems, document.getElementById('react-application'));
