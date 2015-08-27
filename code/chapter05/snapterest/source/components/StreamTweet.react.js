a16d6905869d9c8c7852320d8f88cb827276167c
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2015-08-28 00:04:51 +0100

    Add Chapter 5 code

---
+++ code/chapter05/snapterest/source/components/StreamTweet.react.js
@@ -0,0 +1,58 @@
+var React = require('react');
+var Header = require('./Header.react');
+var Tweet = require('./Tweet.react');
+
+var StreamTweet = React.createClass({
+
+  getInitialState: function () {
+    console.log('[Snapterest] StreamTweet: 1. Running getInitialState()');
+    
+    return {
+      numberOfCharactersIsIncreasing: null,
+      headerText: null
+    };
+  },
+
+  componentWillMount: function () {
+    console.log('[Snapterest] StreamTweet: 2. Running componentWillMount()');
+    
+    this.setState({
+      numberOfCharactersIsIncreasing: true,
+      headerText: 'Latest public photo from Twitter'
+    });
+
+    window.snapterest = {
+      numberOfReceivedTweets: 1,
+      numberOfDisplayedTweets: 1
+    };
+  },
+  
+  componentDidMount: function () {
+    console.log('[Snapterest] StreamTweet: 3. Running componentDidMount()');
+    
+    var componentDOMRepresentation = ReactDOM.findDOMNode(this);
+    window.snapterest.headerHtml = componentDOMRepresentation.children[0].outerHTML;
+    window.snapterest.tweetHtml = componentDOMRepresentation.children[1].outerHTML;
+  },
+
+  componentWillUnmount: function () {
+    console.log('[Snapterest] StreamTweet: 8. Running componentWillUnmount()');
+    
+    delete window.snapterest;
+  },
+
+  render: function () {
+    console.log('[Snapterest] StreamTweet: Running render()');
+    
+    return (
+      <section>
+        <Header text={this.state.headerText} />
+        <Tweet
+          tweet={this.props.tweet}
+          onImageClick={this.props.onAddTweetToCollection} />
+      </section>
+    ); 
+  }
+});
+
+module.exports = StreamTweet;
